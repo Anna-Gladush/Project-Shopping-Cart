fd9a029052b9606835abefd81b26c0e33b94cece
Anna-Gladush <ann.gladush@gmail.com>
2026-08-29 08:54:34 +0200

    Fixing problem with marker img and airQualityTooltip showing correctly on a page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
 // import { data } from './data';
-// import { useEffect, useState} from 'react';
+// import { useEffect } from 'react';
 // import { About } from "./components/About";
 // import { Homepage } from "./components/Home";
 // import { Shop } from "./components/Shop";
 // import { Cart } from "./components/Cart";
 import { useTranslation } from 'react-i18next';
+import { Discojs } from 'discojs'
 import type { JSX } from 'react/jsx-runtime';
-
+import { useEffect, useState } from 'react';
+// import useDiscoData from './customHooks/useDiscoData';
 
 function App(): JSX.Element {
   const { i18n, t } = useTranslation("home")
   // const [cart, setCart] = useState([]);
+  // const {data, loading, error, fetchAlbumData} = useDiscoData()
+  const [ products, setProducts ] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   const handleChangeLanguage = () => {
     const newLanguage = i18n.language === "ru" ? "en" : "ru";
     i18n.changeLanguage(newLanguage);
   }
+
+
+  useEffect(() => {
+    if (loading) {
+      const client = new Discojs({
+        userToken: import.meta.env.VITE_USER_TOKEN,
+      })
+      client
+        .getWantlistForUser("kosmira")
+        .then(data => {
+          console.log(data.wants)
+          setProducts(data.wants)
+          setLoading(false);
+        })
+        // .then(data => {
+        //   setProducts(data.items)
+        //   console.log(data.items)
+        // })
+    }
+  }, [loading])
+  if (loading) return <p>Loading...</p>;
+  // if (error) return <p>A network error was encountered</p>;
+
   return (
     <>
       {/* <About */}
@@ -23,6 +51,17 @@ function App(): JSX.Element {
       {/* <Shop products={data}/> */}
       {/* <Cart cart={[]}/> */}
       <button onClick={handleChangeLanguage}>{t("changeLang")}</button>
+      {products.map(album => {
+        return (
+          <>
+          <li key={`${album.id}`}>
+            <p>{album.basic_information.title}</p>
+            <img src={album.basic_information.thumb} alt={`album cover of ${album.basic_information.title}`} />
+          </li>
+          </>
+          
+        )
+      })}
     </>
   )
 }
